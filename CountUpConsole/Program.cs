c8ab04aa7f5a7b47a78395f65f55602a876e20c1
Mage <John.Lawson@TLMAGE.net>
2014-12-18 16:55:22 -0700

    First baselevel

---
+++ CountUpConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace net.tlmage.clearmeasure.exercise
+{
+    class CountUpConsole
+    {
+        static void Main(string[] args)
+        {
+            String result = CountUpUtility.CountUp(100);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CountUpUtility/CountUpUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace net.tlmage.clearmeasure.exercise
+{
+    public class CountUpUtility
+    {
+        public static String CountUp(int upperBound)
+        {
+            String result = "";
+            for (int i = 1; i <= upperBound; i++)
+            {
+                bool isDivisibleBy3 = i % 3 == 0;
+                bool isDivisibleBy5 = i % 5 == 0;
+
+                if (isDivisibleBy3 && isDivisibleBy5)
+                    result += "FizzBuzz\n";
+                else if (isDivisibleBy3)
+                    result += "Fizz\n";
+                else if (isDivisibleBy5)
+                    result += "Buzz\n";
+                else
+                    result += String.Format("{0}\n", i);
+            }
+            return result;
+        }
+
+    }
+}
