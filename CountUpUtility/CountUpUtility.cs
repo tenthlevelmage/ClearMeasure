997b061fefd6061b1b73b0e7b8f82ed901a59279
Mage <John.Lawson@TLMAGE.net>
2014-12-18 17:32:25 -0700

    Keep the original interface, but add new, more-flexible interfaces

--- CountUpUtility/CountUpUtility.cs
+++ CountUpUtility/CountUpUtility.cs
@@ -1,27 +1,63 @@
 ﻿using System;
+using System.IO;
 
 namespace net.tlmage.clearmeasure.exercise
 {
     public class CountUpUtility
     {
-        public static String CountUp(int upperBound)
+        // Print all the integers between 1 and 100 (inclusive).
+        // Except, print "Fizz" when the integer is evenly divisible by 3,
+        // print "Buzz" when the integer is evenly divisible by 5;
+        // print "FizzBUzz" when the integer is evenly divisible by both
+        public static void CountUp()
         {
-            String result = "";
-            for (int i = 1; i <= upperBound; i++)
+            CountUp(100);
+        }
+
+        // Print all the integers between 1 and a specified value (inclusive).
+        // Except, print "Fizz" when the integer is evenly divisible by 3,
+        // print "Buzz" when the integer is evenly divisible by 5;
+        // print "FizzBUzz" when the integer is evenly divisible by both
+        public static void CountUp(int upperBound)
+        {
+            String result = CountUp(1,upperBound);
+            System.Console.WriteLine("{0}\n", result);
+        }
+
+        // Print all the integers between a lowerBound and and upperBound (inclusive).
+        // Except, print "Fizz" when the integer is evenly divisible by 3,
+        // print "Buzz" when the integer is evenly divisible by 5;
+        // print "FizzBUzz" when the integer is evenly divisible by both
+        public static String CountUp(int lowerBound, int upperBound)
+        {
+            StringWriter stringWriter = new StringWriter();
+            CountUp(lowerBound, upperBound, stringWriter);
+
+            String result = stringWriter.ToString();
+            return result;
+        }
+
+        // Print all the integers between a lowerBound and and upperBound (inclusive).
+        // Except, print "Fizz" when the integer is evenly divisible by 3,
+        // print "Buzz" when the integer is evenly divisible by 5;
+        // print "FizzBUzz" when the integer is evenly divisible by both
+        // Return the result through a passed-in TextWriter
+        public static void CountUp(int lowerBound, int upperBound, TextWriter textWriter)
+        {
+            for (int i = lowerBound; i <= upperBound; i++)
             {
-                bool isDivisibleBy3 = i % 3 == 0;
-                bool isDivisibleBy5 = i % 5 == 0;
+                bool isDivisibleBy3 = i%3 == 0;
+                bool isDivisibleBy5 = i%5 == 0;
 
                 if (isDivisibleBy3 && isDivisibleBy5)
-                    result += "FizzBuzz\n";
+                    textWriter.WriteLine("FizzBuzz");
                 else if (isDivisibleBy3)
-                    result += "Fizz\n";
+                    textWriter.WriteLine("Fizz");
                 else if (isDivisibleBy5)
-                    result += "Buzz\n";
+                    textWriter.WriteLine("Buzz");
                 else
-                    result += String.Format("{0}\n", i);
+                    textWriter.WriteLine("{0}", i);
             }
-            return result;
         }
 
     }
